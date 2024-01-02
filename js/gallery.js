4a5df8651567dd6de700650e711a505eb3230c43
scolorado <224scolorado@frshd.com>
2024-01-02 11:44:55 -0500

    finished object constructor slide

--- js/gallery.js
+++ js/gallery.js
@@ -54,7 +54,7 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
+var mUrl = images.JSON;
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
@@ -85,4 +85,13 @@ function GalleryImage() {
 	//2. description of photo
 	//3. the date when the photo was taken
 	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+
+	let locatation;
+	let description;
+	let date;
+	let img;
+}
+
+function fetchJSON(){
+
 }
