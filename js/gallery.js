525ed61e901c7998aa61f9861c74d6d961b3d97e
scolorado <224scolorado@frshd.com>
2024-01-08 11:49:53 -0500

    added new img and reverted json file

--- js/gallery.js
+++ js/gallery.js
@@ -67,7 +67,7 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'extra.json';
+var mUrl = 'images.json';
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
